925f814d617c725580cdcb7a6d2df2dc5efde41c
Pierre <pierre.gf@hotmail.fr>
2020-04-24 18:30:35 +0200

    reset password

---
+++ src/Form/ResetPasswordType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ResetPasswordType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+        ->add('plainPassword', RepeatedType::class, array(
+            'type' => PasswordType::class,
+            'first_options'  => ['label' => 'Mot de passe'],
+            'second_options' => ['label' => 'Confirmez votre mot de passe'],
+            'invalid_message' => "Les mots de passe ne correspondent pas."
+        ))
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            // Configure your form options here
+        ]);
+    }
+}
