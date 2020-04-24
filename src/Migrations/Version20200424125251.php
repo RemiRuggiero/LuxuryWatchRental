38a1286b641d7ef9ab1b1aa51fc42bc24b14f414
Matthieu Pommery <pommerymatthieu.pro@gmail.com>
2020-04-24 14:53:49 +0200

    new migration v2

--- src/Migrations/Version20200424125251.php
+++ src/Migrations/Version20200424125251.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20200424123825 extends AbstractMigration
+final class Version20200424125251 extends AbstractMigration
 {
     public function getDescription() : string
     {
