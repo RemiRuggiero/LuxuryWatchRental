b1bf27bea1d3810312a7fef49cbbe76c6c13b2d3
Walid Bilem <wldblm@icloud.com>
2020-04-25 20:05:57 +0200

    -

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -25,7 +25,7 @@ class UserController extends AbstractController
     public function register( Request $request, UserPasswordEncoderInterface $encoder, EntityManagerInterface $em, GuardAuthenticatorHandler $guardHandler, Swift_Mailer $mailer) : Response
     {
         $user = new User();
-        $form = $this->createForm( RegisterType::class, $user );
+        $form = $this->createForm( RegisterType::class, $user);
         $form->handleRequest( $request );
 
         if( $form->isSubmitted() && $form->isValid() ){
