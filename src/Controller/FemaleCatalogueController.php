bb3f9eb20d525d2baf1ca25674be47c385513b4b
Pierre <pierre.gf@hotmail.fr>
2020-04-28 15:40:12 +0200

    fix catalogue h/f

---
+++ src/Controller/FemaleCatalogueController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use App\Entity\WatchModel;
+use App\Service\watchlistService;
+use App\Repository\WatchModelRepository;
+
+class FemaleCatalogueController extends AbstractController
+{
+
+
+    private $watchlistService;
+    
+
+    public function __construct( watchlistService $watchlistService){
+        $this->watchlistService = $watchlistService;
+        
+    }
+
+
+    /**
+     * @Route("/female/catalogue", name="female_catalogue")
+     */
+
+    public function femalewatchlist(Request $request)
+    {
+        $watches = $request->query->get('list');
+       
+        $maleWatches = $this->watchlistService->listF();
+
+        return $this->render( 'female_catalogue/femaleCatalogue.html.twig', array(
+            'watches' => $maleWatches,
+            
+        ));
+         
+
+    }
+
+    
+}
