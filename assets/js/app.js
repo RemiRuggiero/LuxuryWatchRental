2623fa386c08109c2a697d2189c4befc0a93bf81
Matthieu Pommery <pommerymatthieu.pro@gmail.com>
2020-05-10 11:32:21 +0200

    transition en cours

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,16 +8,16 @@ import '../css/account.scss';
 import '../css/show.scss';
 import '../css/contact.scss';
 import '../css/cart.scss';
+import '../css/transition.scss';
 
 
 
-
-// Transition de page
 
 // Page: "show.html.twig" -> Gestion transition image du carousel
 $('.carousel').carousel({
   interval: 6200
 })
+
 
 // Page: "show.html.twig" -> Calendrier DateRangePicker
 $(function() {
@@ -71,3 +71,65 @@ $(function() {
   
 
 });
+
+// Transition de page
+import Swup from 'swup';
+
+function init() {
+  if (document.querySelector()) {
+    $('input[name="daterange"]').daterangepicker({
+      opens: 'right',
+      minDate: new Date(currentYear, currentMonth, currentDate +3),
+      isInvalidDate: function(date){
+        if(disabledDate.length == 0){
+          true
+        }
+        else{
+          var disabled_start = moment(disabledDate[0].date).add(-1, 'day');
+        var disabled_end = moment(disabledDate[disabledDate.length-1].date).add(1, 'day');
+        return date.isAfter(disabled_start) && date.isBefore(disabled_end);  
+        }
+        
+      }
+      
+    }, function(start, end, label) {
+       console.log("A new date selection was made: " + start.format('dd/MMMM/y') + ' to ' + end.format('dd/MMMM/y'));
+    });
+  }
+}
+
+const swup = new Swup()
+
+init();
+
+swup.on('contentReplaced', init);
+
+
+function unload() {
+  if (document.querySelector()) {
+    $('input[name="daterange"]').daterangepicker({
+      opens: 'right',
+      minDate: new Date(currentYear, currentMonth, currentDate +3),
+      isInvalidDate: function(date){
+        if(disabledDate.length == 0){
+          true
+        }
+        else{
+          var disabled_start = moment(disabledDate[0].date).add(-1, 'day');
+        var disabled_end = moment(disabledDate[disabledDate.length-1].date).add(1, 'day');
+        return date.isAfter(disabled_start) && date.isBefore(disabled_end);  
+        }
+        
+      }
+      
+    }, function(start, end, label) {
+       console.log("A new date selection was made: " + start.format('dd/MMMM/y') + ' to ' + end.format('dd/MMMM/y'));
+    });
+  }
+  }
+  
+swup.on('willReplaceContent', unload);
+
+
+
+
